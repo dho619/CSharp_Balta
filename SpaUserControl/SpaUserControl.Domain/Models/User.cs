f215beddedc4eabf2de0365ec5e7322d84e9ec79
Geovane Barbosa <gbarbosa@telluria.com.br>
2022-02-01 18:12:37 -0300

    Projeto SpaUserControl: Modelando o Domínio

---
+++ SpaUserControl/SpaUserControl.Domain/Models/User.cs
@@ -0,0 +1,57 @@
+﻿using SpaUserControl.Common.Resources;
+using SpaUserControl.Common.Validation;
+using System;
+
+namespace SpaUserControl.Domain.Models
+{
+    public class User
+    {
+        #region Ctor
+        public User(string name, string email)
+        {
+            Id = Guid.NewGuid();
+            Name = name;
+            Email = email;
+        }
+        #endregion
+
+        #region Properties
+        public Guid Id { get; private set; }
+        public string Name { get; private set; }
+        public string Email { get; private set; }
+        public string Password { get; private set; }
+        #endregion
+
+        #region Methods
+
+        public void SetPassword(string password, string confirmPassword)
+        {
+            AssertionConcern.AssertArgumentNotNull(password, Errors.InvalidPassword);
+            AssertionConcern.AssertArgumentNotNull(confirmPassword , Errors.InvalidPasswordConfirmation);
+            AssertionConcern.AssertArgumentEquals(password, confirmPassword, Errors.PasswordDoNotMatch);
+            AssertionConcern.AssertArgumentLength(password, 6, 20, Errors.InvalidPassword);
+
+            this.Password = PasswordAssertionConcern.Encrypt(password);
+        }
+
+        public string ResetPassword()
+        {
+            string password = Guid.NewGuid().ToString().Substring(0, 8);
+            this.Password = PasswordAssertionConcern.Encrypt(password);
+            return password;
+        }
+
+        public void ChangeName(string name)
+        {
+            this.Name = name;
+        }
+
+        public void Validate()
+        {
+            AssertionConcern.AssertArgumentLength(this.Name, 3, 250, Errors.InvalidUserName);
+            EmailAssertionConcern.AssertIsValid(this.Email);
+            PasswordAssertionConcern.AssertIsValid(this.Password);
+        }
+        #endregion
+    }
+}
