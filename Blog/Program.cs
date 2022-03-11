145af7a4f96979d81cebe75ed873324047dd3a7b
Geovane Aparecido Barbosa <gbarbosa@topsys.com>
2022-03-11 10:14:37 -0300

    Projeto Blog: Módulo 2 Completado

--- Blog/Program.cs
+++ Blog/Program.cs
@@ -10,45 +10,58 @@ namespace Blog
     {
         static void Main(string[] args)
         {
-            using (var context = new BlogDataContext())
-            {
-                //CREATE
-                // var tag = new Tag { Name="ASP.NET", Slug = "aspnet"};
-                // context.Tags.Add(tag);
-                // context.SaveChanges();
+            using var context = new BlogDataContext();            
 
-                //UPDATE
-                // var tag = context.Tags.FirstOrDefault(x=> x.Id == 1);
-                // tag.Name = ".NET";
-                // tag.Slug = "dotnet";
-                // context.Update(tag);
-                // context.SaveChanges();
+            // var user = new User
+            // {
+            //     Name = "Teste",
+            //     Slug = "teste",
+            //     Email = "teste@teste.com",
+            //     Bio = "teste de bio",
+            //     Image = "https://balta.io",
+            //     PasswordHash = "123456789"
+            // };
 
-                //DELETE
-                // var tag = context.Tags.FirstOrDefault(x=> x.Id == 1);
-                // context.Remove(tag);
-                // context.SaveChanges();
-                
-                // var tags = context
-                //     .Tags
-                //     .AsNoTracking() //REMOVER METADADOS
-                //     //.Where(x => x.Name.Contains(".NET"))
-                //     .ToList();
+            // var category = new Category{Name="Backend", Slug="backend"};
 
-                // foreach (var tag in tags)
-                // {
-                //     Console.WriteLine(tag.Name);
-                // }
+            // var post = new Post
+            // {
+            //     Author=user,
+            //     Category=category,
+            //     Body="<p>Hello Word</p>",
+            //     Slug="comecando-com-ef-core",
+            //     Summary="Neste artigo vamos aprender EF core",
+            //     Title="Começando com EF Core",
+            //     CreateDate = DateTime.Now,
+            //     LastUpdateDate = DateTime.Now           
+            // };
 
-                // var tag = context
-                //             .Tags
-                //             .AsNoTracking()
-                //             .FirstOrDefault(x=> x.Id == 2);
-                // Console.WriteLine(tag?.Name); //"?" é que como é pra fim o primeiro ou o default, 
-                //                              //se não achar o default é nullo, logo o "?" é para não dar erro
+            // context.Posts.Add(post);
+            // context.SaveChanges();
+
+            // var posts = context
+            //     .Posts
+            //     .AsNoTracking()
+            //     .Include(x => x.Author)
+            //     .Include(x => x.Category)
+            //     //    .ThenInclude(x => x.) //faz um subselect
+            //     .OrderBy(x=> x.LastUpdateDate)
+            //     .ToList();
+            // foreach (var post in posts)
+            // {
+            //     Console.WriteLine($"{post.Title} escrito por {post.Author?.Name}");
+            // }
+
+            var post = context
+                .Posts
+                .Include(x => x.Author)
+                .Include(x => x.Category)
+                .OrderByDescending(x=> x.LastUpdateDate)
+                .FirstOrDefault(); //Pegando o primeiro
             
-            }
-            
+            post.Author.Name = "Teste Jr";
+            context.Posts.Update(post);
+            context.SaveChanges();
         }
     }
 }
