c903b401a3dddf6d6b3b53aaf44072fcf3730c3d
Geovane Aparecido Barbosa <gbarbosa@topsys.com>
2022-03-11 14:21:13 -0300

    Projeto Blog: Módulo 4 Completado

--- Blog/Data/BlogDataContext.cs
+++ Blog/Data/BlogDataContext.cs
@@ -13,7 +13,7 @@ namespace Blog.Data
 
         protected override void OnConfiguring(
             DbContextOptionsBuilder options) {
-                options.UseSqlServer("Server=localhost,1433;Database=Blog;User ID=sa;Password=1q2w3e4r@#$");
+                options.UseSqlServer("Server=localhost,1433;Database=Blog2;User ID=sa;Password=1q2w3e4r@#$");
                 //options.LogTo(Console.WriteLine);
             }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
--- Blog/Program.cs
+++ Blog/Program.cs
@@ -12,30 +12,17 @@ namespace Blog
         {
             using var context = new BlogDataContext();            
 
-            // context.Users.Add(new User
-            // {
-            //     Name= "Marcos Teste",
-            //     Bio = "9x Microsoft MVP",
-            //     Email = "marcos@teste.com",
-            //     Image = "https://balta.io",
-            //     PasswordHash = "1234",
-            //     Slug = "marcos-teste"
-            // });
-
-            var user = context.Users.FirstOrDefault();
-            var post = new Post {
-                Author = user,
-                Body = "Meu artigo",
-                Category = new Category{
-                    Name="Backend mvp",
-                    Slug="backend-mvp"
-                },
-                CreateDate = System.DateTime.Now,
-                Slug = "meu-artigo",
-                Summary = "Neste artigo vamos conferir",
-                Title = "Meu artigo",
-            };
-            context.Posts.Add(post);
+            context.Users.Add(new User
+            {
+                Name= "Pedro Teste",
+                Bio = "MVP",
+                Email = "pedro@teste.com",
+                Image = "https://balta.io",
+                PasswordHash = "1234",
+                Slug = "pedro-teste",
+                GitHub= "pedro123"
+            });
+            
             context.SaveChanges();
         }
     }
